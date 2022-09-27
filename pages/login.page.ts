6d70e89bf603568b396c7f3a813493c0f0ebe635
Alvaro Navega <alvaroflaviano@hotmail.com>
2022-09-27 19:24:02 -0300

    add locally secrets

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,6 +1,8 @@
 import { test, expect, Locator, Page } from '@playwright/test';
+import { secrets } from '../fixtures/secrets.fixture';
 
 export class loginPage {
+    readonly sec = new secrets();
     readonly username: Locator;
     readonly password: Locator;
     readonly btnEntrar: Locator;
@@ -13,7 +15,7 @@ export class loginPage {
         this.password = page.locator('#j_password');
         this.btnEntrar = page.locator('#submit');
         this.title = page.locator('.titleLogin');
-        this.url = 'http://tjsp-itg.ungp.softplan.com.br/sajadm/';
+        this.url = this.sec.url;
     }
     async login(username: string, password: string) {
         await this.page.goto(this.url);
--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { loginPage } from '../pages/login.page';
-
+import { secrets } from '../fixtures/secrets.fixture';
 test.beforeEach(async ({ page }, testInfo) => {
+  let sec = new secrets();
   const loginpage = new loginPage(page);
   console.log(`Running ${testInfo.title}`);
-  await loginpage.login('sajadm', '123');
+  await loginpage.login(sec.username, sec.password);
 });
 
 test.describe('Validação do processo', () => {
