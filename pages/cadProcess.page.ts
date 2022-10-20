ff1a017e69498fa54bd068cd4ba6b34f67876849
Alvaro Navega <alvaroflaviano@hotmail.com>
2022-10-20 10:32:08 -0300

    Atualizacao PageObject

--- pages/cadProcess.page.ts
+++ pages/cadProcess.page.ts
@@ -1,7 +1,8 @@
 import { test, expect, Locator, Page } from '@playwright/test';
 export class cadProcessPage {
+    readonly menuCPA: Locator;
     readonly menuCadProcesso: Locator;
-    readonly pesqAssunto: Locator;
+    readonly assunto: Locator;
     readonly tipoProcesso: Locator;
     readonly palavraChave: Locator;
     readonly tarjas: Locator;
@@ -14,10 +15,11 @@ export class cadProcessPage {
     readonly page: Page;
     constructor(page: Page) {
         this.page = page;
-        this.menuCadProcesso = page.locator('#cadastroProcesso');
-        this.pesqAssunto = page.locator('#pesqAssunto');
+        this.menuCPA = page.frameLocator('[aria-label="menu"]').locator('#link_Modulo_64_1 >> text=CPA');
+        this.menuCadProcesso = page.frameLocator('[aria-label="menu"]').locator('#itemMenu_64_1_2029');
+        this.assunto = page.frameLocator('iframe[name="page"]').locator('text=Assunto principal:​​ >> [aria-label="Abrir modal de consulta"]');
         this.tipoProcesso = page.locator('#tipoProcesso');
-        this.palavraChave = page.locator('#palavraChave');
+        this.palavraChave = page.frameLocator('iframe[name="page"]').locator('#tags div:has-text("Palavra chave")');
         this.tarjas = page.locator('#tarjas');
         this.municipio = page.locator('#municipio');
         this.nomeInteressado = page.locator('#nomeInteressado');
@@ -26,5 +28,24 @@ export class cadProcessPage {
         this.bntConcuir = page.locator('#btnConcluir');
         this.bntLimpar = page.locator('#btnLimpar');
     }
+    async acessoCadastroProcesso() {
+        await this.menuCPA.click();
+        await this.menuCadProcesso.click();
+   }
+    async assuntoFill(assunto: string) {
+        await this.assunto.click();
+        // asunto
+        // '[aria-label="Código\\: 1\\, Assunto\\: Acesso à internet\\, correio eletrônico e convênios\\, Grupo\\: Solicitação de Acesso aos Sistemas\\, "]'
+        await this.page.frameLocator('iframe[name="page"]').locator(assunto).check();
+        await this.page.frameLocator('iframe[name="page"]').locator('button:has-text("Selecionar")').click();
+    }
+    async palavraChaveFill(palavraChave: string) {
+        this.palavraChave.click();
+        //palavrachave = AUROMACAO
+        await this.palavraChave.fill(palavraChave);
+        await this.page.frameLocator('iframe[name="page"]').locator('#react-select-4-option-0').click()
+    }
+
+
 
 }
--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -2,28 +2,40 @@ import { test, expect } from '@playwright/test';
 import { loginPage } from '../pages/login.page';
 import { secrets } from '../fixtures/secrets.fixture';
 import { perfilPage } from '../pages/perfil.page';
+import { cadProcessPage } from '../pages/cadProcess.page';
 
 test.beforeEach(async ({ page }, testInfo) => {
   const sec = new secrets();
   const loginpage = new loginPage(page);
   console.log(`Running ${testInfo.title}`);
   await loginpage.login(sec.userAdm, sec.passAdm);
- // await page.context().storageState({ path: 'storageState.json' });
+  await page.context().storageState({ path: 'storageState.json' });
 
 });
 
-test.afterAll(async ({ page }) => {
-  const fs = require('fs');
-  const filePath = 'storageState.json';
-  fs.unlinkSync(filePath);
-  await page.context().clearCookies();
-});
+//test.afterAll(async ({ page }) => {
+//  const fs = require('fs');
+//  const filePath = 'storageState.json';
+//  fs.unlinkSync(filePath);
+//  await page.context().clearCookies();
+//});
+
 test.describe('Criar e validar processo', () => {
-  test('Validar perfil', async ({ page }) => {
+  test.skip('Validar perfil', async ({ page }) => {
     const perfilpage = new perfilPage(page);
     await perfilpage.menuSec.click();
     await perfilpage.dropdownPerfil.click();
     await perfilpage.menuUsuarioPerfil.click();
+
+  })
+  test('Criar process', async ({ page }) => {
+    //const sec = new secrets();
+    // loginpage = new loginPage(page);
+    //await loginpage.login(sec.userAdm, sec.passAdm);
+    const cadproceso = new cadProcessPage(page);
+    await cadproceso.acessoCadastroProcesso();
+
+    cadproceso.assuntoFill('[aria-label="Código\\: 1\\, Assunto\\: Acesso à internet\\, correio eletrônico e convênios\\, Grupo\\: Solicitação de Acesso aos Sistemas\\, "]');
 
   })
 });
